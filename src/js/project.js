08ae5967290cd23d2e5dfb119735ae5bd4cc1cfe
Greg Johnston <gregmj.sco@gmail.com>
2023-09-03 11:09:59 +0900

    Add Todo and Projct Class

--- src/js/project.js
+++ src/js/project.js
@@ -1,1 +1,21 @@
-import '../style.css';
+import './style.css';
+
+class Project {
+    constructor(name) {
+      this.name = name;
+      this.todos = [];
+    }
+  
+    addTodo(todo) {
+      this.todos.push(todo);
+    }
+  
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    }
+  
+    // You can add more methods as needed, such as getting a specific todo, listing todos, etc.
+  }
---
+++ src/js/todo.js
@@ -0,0 +1,32 @@
+import '../css/style.css'
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.completed = false; 
+    }
+  
+    markAsCompleted() {
+      this.completed = true;
+    }
+  
+    editTitle(newTitle) {
+      this.title = newTitle;
+    }
+  
+    editDescription(newDescription) {
+      this.description = newDescription;
+    }
+  
+    editDueDate(newDueDate) {
+      this.dueDate = newDueDate;
+    }
+  
+    editPriority(newPriority) {
+      this.priority = newPriority;
+    }
+  }
+  
